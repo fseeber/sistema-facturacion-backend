938f5cf22a299a446c30c59851fa9d63baa0615e
Federico Seeber <seeberfederico@gmail.com>
2019-11-21 15:46:36 -0300

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,22 @@
+insert into regiones (id, nombre) VALUES (1, 'Sudamérica');
+insert into regiones (id, nombre) VALUES (2, 'Centroamérica');
+insert into regiones (id, nombre) VALUES (3, 'Norteamérica');
+insert into regiones (id, nombre) VALUES (4, 'Europa');
+insert into regiones (id, nombre) VALUES (5, 'Asia');
+insert into regiones (id, nombre) VALUES (6, 'Oceanía');
+insert into regiones (id, nombre) VALUES (7, 'Antártida');
+
+insert into clientes(region_id, nombre, apellido, email, create_at) values (1, 'Andres','Guzman','profesora@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (2, 'Federico','Marcelo','profesor1@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (4, 'Marcelo','Marcelo','Marcelo@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (4, 'Antonio','Guzman','Guzman@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (1, 'Pedro','Jose','profesor@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (3, 'Jose','Guzman','Guzman2@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (5, 'Mario','Guzman','profesor3@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (6, 'Andres','Guzman','profesor4@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (1, 'Andres','Mario','profesor5@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (7, 'Antonio','Mario','profesor6@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (1, 'Federico','Seeber','seeberfederico7@gmail.com','2019-10-09');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (2, 'Andres','Guzman','profesora@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (2, 'Federico','Marcelo','profesor1@gmail.com','2018-01-01');
+insert into clientes(region_id, nombre, apellido, email, create_at) values (2, 'Marcelo','Marcelo','Marcelo@gmail.com','2018-01-01');
