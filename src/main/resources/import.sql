aefea0df45ec6599d403345b15bb8b39d76cad11
fseeber <fseeber@fleni.org.ar>
2020-02-11 16:54:07 -0300

    agregos entidades

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,33 +1,54 @@
-insert into regiones (id, nombre) VALUES (1, 'Sudamérica');
-insert into regiones (id, nombre) VALUES (2, 'Centroamérica');
-insert into regiones (id, nombre) VALUES (3, 'Norteamérica');
-insert into regiones (id, nombre) VALUES (4, 'Europa');
-insert into regiones (id, nombre) VALUES (5, 'Asia');
-insert into regiones (id, nombre) VALUES (6, 'Oceanía');
-insert into regiones (id, nombre) VALUES (7, 'Antártida');
+/* Populate tabla clientes */
 
-insert into clientes(region_id, nombre, apellido, email, create_at) values (1, 'Andres','Guzman','profesora@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (2, 'Federico','Marcelo','profesor1@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (4, 'Marcelo','Marcelo','Marcelo@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (4, 'Antonio','Guzman','Guzman@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (1, 'Pedro','Jose','profesor@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (3, 'Jose','Guzman','Guzman2@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (5, 'Mario','Guzman','profesor3@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (6, 'Andres','Guzman','profesor4@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (1, 'Andres','Mario','profesor5@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (7, 'Antonio','Mario','profesor6@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (1, 'Federico','Seeber','seeberfederico7@gmail.com','2019-10-09');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (2, 'Andres','Guzman','profesora@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (2, 'Federico','Marcelo','profesor1@gmail.com','2018-01-01');
-insert into clientes(region_id, nombre, apellido, email, create_at) values (2, 'Marcelo','Marcelo','Marcelo@gmail.com','2018-01-01');
+INSERT INTO regiones (id, nombre) VALUES (1, 'Sudamérica');
+INSERT INTO regiones (id, nombre) VALUES (2, 'Centroamérica');
+INSERT INTO regiones (id, nombre) VALUES (3, 'Norteamérica');
+INSERT INTO regiones (id, nombre) VALUES (4, 'Europa');
+INSERT INTO regiones (id, nombre) VALUES (5, 'Asia');
+INSERT INTO regiones (id, nombre) VALUES (6, 'Africa');
+INSERT INTO regiones (id, nombre) VALUES (7, 'Oceanía');
+INSERT INTO regiones (id, nombre) VALUES (8, 'Antártida');
 
-/* Creamos algunos usuarios con sus roles*/
-insert into usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres', '$2a$10$oYBmPcdVLCh.A85Bjq3aRu40eUW7o.QD4esNSwAdYSWl88fgRQddO', 1, 'Federico', 'Seeber', 'seeberfederico@gmail.com');
-insert into usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$U9VwL47095p2GVVyb7qeaeo9l1ibIBt3heP2JaZ6OLTwF31TFoD1O', 2, 'Fulano', 'Mengano', 'fulano@hotmail.com');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(1, 'Andrés', 'Guzmán', 'profesor@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(2, 'Mr. John', 'Doe', 'john.doe@gmail.com', '2018-01-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(4, 'Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2018-01-03');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(4, 'Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2018-01-04');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(4, 'Erich', 'Gamma', 'erich.gamma@gmail.com', '2018-02-01');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(3, 'Richard', 'Helm', 'richard.helm@gmail.com', '2018-02-10');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(3, 'Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2018-02-18');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(3, 'John', 'Vlissides', 'john.vlissides@gmail.com', '2018-02-28');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(3, 'Dr. James', 'Gosling', 'james.gosling@gmail.com', '2018-03-03');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(5, 'Magma', 'Lee', 'magma.lee@gmail.com', '2018-03-04');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(6, 'Tornado', 'Roe', 'tornado.roe@gmail.com', '2018-03-05');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(7, 'Jade', 'Doe', 'jane.doe@gmail.com', '2018-03-06');
 
-insert into roles (nombre) VALUES ('ROLE_USER');
-insert into roles (nombre) VALUES ('ROLE_ADMIN');
+/* Creamos algunos usuarios con sus roles */
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$C3Uln5uqnzx/GswADURJGOIdBqYrly9731fnwKDaUdBkt/M3qvtLq',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$RmdEsvEfhI7Rcm9f/uZXPebZVCcPC7ZXZwV51efAvMAp1rIaRAfPK',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
 
-insert into usuarios_roles (usuario_id, role_id) VALUES (1, 1);
-insert into usuarios_roles (usuario_id, role_id) VALUES (2, 2);
-insert into usuarios_roles (usuario_id, role_id) VALUES (2, 1);
+INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
+
+/* Populate tabla productos */
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic Pantalla LCD', 259990, CURRENT_TIMESTAMP();
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony Camara digital DSC-W320B', 123490, CURRENT_TIMESTAMP();
+INSERT INTO productos (nombre, precio, create_at) VALUES('Apple iPod shuffle', 1499990, CURRENT_TIMESTAMP();
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony Notebook Z110', 37990, CURRENT_TIMESTAMP();
+INSERT INTO productos (nombre, precio, create_at) VALUES('Hewlett Packard Multifuncional F2280', 69990, CURRENT_TIMESTAMP));
+INSERT INTO productos (nombre, precio, create_at) VALUES('Bianchi Bicicleta Aro 26', 69990, CURRENT_TIMESTAMP();
+INSERT INTO productos (nombre, precio, create_at) VALUES('Mica Comoda 5 Cajones', 299990, CURRENT_TIMESTAMP();
+
+/* Creamos algunas facturas */
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura equipos de oficina', null, 1, CURRENT_TIMESTAMP();
+
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 1);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(2, 1, 4);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 5);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 7);
+
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura Bicicleta', 'Alguna nota importante!', 1, CURRENT_TIMESTAMP();
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(3, 2, 6);
